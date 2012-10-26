1fdf12aa9cf3439c639bb0ddaf0c037961556c38
Terrence Meiczinger <terrence72@gmail.com>
2012-10-25 20:59:10 -0400

    repo cleanup

---
+++ libopendcp/image/opendcp_bmp.c
@@ -0,0 +1,232 @@
+/*
+    OpenDCP: Builds Digital Cinema Packages
+    Copyright (c) 2010-2012 Terrence Meiczinger, All Rights Reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "opendcp.h"
+#include "opendcp_image.h"
+
+#define MAGIC_NUMBER 0x4D42
+
+typedef enum {
+    BMP_R         = 0,
+    BMP_G         = 1,
+    BMP_B         = 2
+} bmp_color_enum;
+
+typedef enum {
+    BMP_TOP         = 0,
+    BMP_BOTTOM      = 1,
+} bmp_row_order_enum;
+
+typedef enum {
+    BMP_RGB       = 0,          /* no compression                 */
+    BMP_RLE8      = 1,          /* 8-bit run-length-encoded       */
+    BMP_RLE4      = 2,          /* 4-bit run-length-encoded       */
+    BMP_BITFIELDS = 3,          /* RGB bitmap                     */
+    BMP_JPEG      = 4,          /* JPEG (not supported)           */
+    BMP_PNG       = 5           /* PNG (not supported)            */
+} bmp_compression_enum;
+
+typedef struct {
+    uint16_t magic_num;         /* magic number 0x4d42            */
+} bmp_magic_num_t;
+
+typedef struct {
+    uint32_t size;              /* file size in bytes             */
+    uint32_t reserved;          /* rervered1, reserved2           */
+    uint32_t offset;            /* offset to image data in bytes  */
+} bmp_file_header_t;
+
+typedef struct {
+    uint32_t header_size;        /* header size in bytes          */
+    int32_t  width;              /* width of image                */
+    int32_t  height;             /* height of image               */
+    uint16_t planes;             /* number of color components    */
+    uint16_t bpp;                /* bits per pixel                */
+    uint32_t compression;        /* compression method            */
+    uint32_t image_size;         /* image size in bytes           */
+    int32_t  x_ppm;              /* x pels                        */
+    int32_t  y_ppm;              /* y pels                        */
+    uint32_t colors_used;        /* number of colors              */
+    uint32_t colors_important;   /* number of important colors    */
+} bmp_image_header_t;
+
+typedef struct {
+    bmp_file_header_t        file;
+    bmp_image_header_t       image;
+    int                      row_order;
+} bmp_image_t;
+
+/* BMPs are sometimes bottom to top, so invert if needed */
+static inline int invert_row(bmp_image_t bmp, int index) {
+    int w = bmp.image.width;
+    int h = bmp.image.height;
+
+    if (bmp.row_order == BMP_BOTTOM) {
+      index = (h - index / w) * w + ((index % w) - w);
+    }
+
+    return index;
+}
+
+void print_bmp_header(bmp_image_t *bmp) {
+    dcp_log(LOG_DEBUG,"%-15.15s: file size:    %d","read_bmp",bmp->file.size);
+    dcp_log(LOG_DEBUG,"%-15.15s: data offset:  %d","read_bmp",bmp->file.offset);
+
+    dcp_log(LOG_DEBUG,"%-15.15s: header_size:  %d","read_bmp",bmp->image.header_size);
+    dcp_log(LOG_DEBUG,"%-15.15s: width:        %d","read_bmp",bmp->image.width);
+    dcp_log(LOG_DEBUG,"%-15.15s: height:       %d","read_bmp",bmp->image.height);
+    dcp_log(LOG_DEBUG,"%-15.15s: planes:       %d","read_bmp",bmp->image.planes);
+    dcp_log(LOG_DEBUG,"%-15.15s: bpp:          %d","read_bmp",bmp->image.bpp);
+    dcp_log(LOG_DEBUG,"%-15.15s: compression:  %d","read_bmp",bmp->image.compression);
+    dcp_log(LOG_DEBUG,"%-15.15s: size:         %d","read_bmp",bmp->image.image_size);
+    dcp_log(LOG_DEBUG,"%-15.15s: x:            %d","read_bmp",bmp->image.x_ppm);
+    dcp_log(LOG_DEBUG,"%-15.15s: y:            %d","read_bmp",bmp->image.y_ppm);
+    dcp_log(LOG_DEBUG,"%-15.15s: colors_used:  %d","read_bmp",bmp->image.colors_used);
+    dcp_log(LOG_DEBUG,"%-15.15s: colors_impor: %d","read_bmp",bmp->image.colors_important);
+
+    dcp_log(LOG_DEBUG,"%-15.15s: row_order:    %d","read_bmp",bmp->row_order);
+}
+
+int read_bmp(odcp_image_t **image_ptr, const char *infile, int fd) {
+    bmp_magic_num_t magic;
+    bmp_image_t     bmp;
+    FILE            *bmp_fp;
+    odcp_image_t    *image = 00;
+    int             pixels = 0;
+    int i,w,h;
+
+    /* open bmp using filename or file descriptor */
+    dcp_log(LOG_DEBUG,"%-15.15s: opening bmp file %s","read_bmp",infile);
+
+    if (fd == 0) {
+        bmp_fp = fopen(infile, "rb");
+    } else {
+        bmp_fp = (FILE *)infile;
+    }
+
+    if (!bmp_fp) {
+        dcp_log(LOG_ERROR,"%-15.15s: opening bmp file %s","read_bmp",infile);
+        return OPENDCP_FATAL;
+    }
+
+    if (fread(&magic,sizeof(bmp_magic_num_t),1,bmp_fp) < sizeof(bmp_magic_num_t)) {
+        dcp_log(LOG_ERROR,"%-15.15s: failed to read magic number","read_bmp");
+        return OPENDCP_FATAL;
+    }
+    
+    if (fread(&bmp,sizeof(bmp_image_t),1,bmp_fp) <= sizeof(bmp_image_t)) {
+        dcp_log(LOG_ERROR,"%-15.15s: failed to header","read_bmp");
+        return OPENDCP_FATAL;
+    }
+
+    if (magic.magic_num != MAGIC_NUMBER) {
+         dcp_log(LOG_ERROR,"%s is not a valid BMP file", infile);
+         return OPENDCP_FATAL;
+    }
+
+    if (bmp.image.height < 0) {
+        bmp.row_order = BMP_TOP;
+    } else {
+        bmp.row_order = BMP_BOTTOM;
+    }
+
+    w = bmp.image.width;
+    h = abs(bmp.image.height);
+    pixels = w * h;
+
+    print_bmp_header(&bmp);
+    
+    switch (bmp.image.compression) {
+        case BMP_RGB:
+            break;
+        case BMP_RLE8:
+        case BMP_RLE4:
+        case BMP_BITFIELDS:
+        case BMP_JPEG:
+        case BMP_PNG:
+        default:
+            dcp_log(LOG_ERROR, "Unsupported image compression: %d\n", bmp.image.compression);
+            return OPENDCP_FATAL;
+            break;
+    }
+
+    /* apparently, some applications don't fill in the image size */
+    if (bmp.image.image_size == 0) {
+        bmp.image.image_size = bmp.file.size - sizeof(bmp_magic_num_t) - sizeof(bmp_image_t);
+    }
+
+    if (bmp.image.bpp < 24 || bmp.image.bpp > 32) {
+        dcp_log(LOG_ERROR, "%d-bit depth is not supported.",bmp.image.bpp);
+        return OPENDCP_FATAL;
+    }
+  
+    /* create the image */
+    dcp_log(LOG_DEBUG,"%-15.15s: allocating odcp image","read_bmp");
+    image = odcp_image_create(3,w,h);
+    dcp_log(LOG_DEBUG,"%-15.15s: image allocated","read_bmp");
+
+    fseek(bmp_fp, bmp.file.offset, SEEK_SET);
+
+    /* RGB(A) */
+    if (bmp.image.compression == BMP_RGB) {
+        /* 16-bits per pixel */
+        if (bmp.image.bpp == 16 ) {
+            uint8_t data[2];
+            for (i=0; i<pixels; i++) { 
+                fread(&data,sizeof(data),1,bmp_fp);
+                int p = invert_row(bmp, i);
+                image->component[BMP_B].data[p] = data[0] << 2;
+                image->component[BMP_G].data[p] = data[0] << 4;
+                image->component[BMP_R].data[p] = data[1] << 2;
+            }
+        /* 24-bits per pixel */
+        } else if (bmp.image.bpp == 24 ) {
+            uint8_t data[3];
+            for (i=0; i<pixels; i++) { 
+                fread(&data,sizeof(data),1,bmp_fp);
+                int p = invert_row(bmp, i);
+                image->component[BMP_B].data[p] = data[0] << 4;
+                image->component[BMP_G].data[p] = data[1] << 4;
+                image->component[BMP_R].data[p] = data[2] << 4;
+            }
+        /* 32-bits per pixel */
+        } else if (bmp.image.bpp == 32 ) {
+            uint8_t data[4];
+            for (i=0; i<pixels; i++) { 
+                fread(&data,sizeof(data),1,bmp_fp);
+                int p = invert_row(bmp, i);
+                image->component[BMP_B].data[p] = data[0] << 4;
+                image->component[BMP_G].data[p] = data[1] << 4;
+                image->component[BMP_R].data[p] = data[2] << 4;
+            }
+        }
+    }
+    /* RGB(A) */
+
+    fclose(bmp_fp);
+
+    dcp_log(LOG_DEBUG,"%-15.15s: BMP read complete","read_bmp");
+    *image_ptr = image;
+
+    return OPENDCP_NO_ERROR;
+}
---
+++ libopendcp/image/opendcp_image.c
@@ -0,0 +1,423 @@
+/*
+    OpenDCP: Builds Digital Cinema Packages
+    Copyright (c) 2010-2011 Terrence Meiczinger, All Rights Reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <openjpeg.h>
+#include <tiffio.h>
+#include "opendcp.h"
+#include "opendcp_image.h"
+#include "opendcp_xyz.h"
+
+#define CLIP(m,max)                                 \
+  (m)<0?0:((m)>max?max:(m))
+
+extern int rgb_to_xyz_calculate(odcp_image_t *image, int index);
+extern int rgb_to_xyz_lut(odcp_image_t *image, int index);
+
+/* create opendcp image structure */
+odcp_image_t *odcp_image_create(int n_components, int w, int h) {
+    int x;
+    odcp_image_t *image = 00; 
+
+    image = (odcp_image_t*) malloc(sizeof(odcp_image_t));
+
+    if (image) {
+        memset(image,0,sizeof(odcp_image_t));
+        image->component = (odcp_image_component_t*) malloc(n_components*sizeof(odcp_image_component_t));
+
+        if (!image->component) {
+            dcp_log(LOG_ERROR,"Unable to allocate memory for image components");
+            odcp_image_free(image);
+            return 00;
+        }
+
+        memset(image->component,0,n_components * sizeof(odcp_image_component_t));
+       
+        for (x=0;x<n_components;x++) {
+            image->component[x].component_number = x;
+            image->component[x].data = (int *)malloc((w*h)*sizeof(int));
+            if (!image->component[x].data) {
+                dcp_log(LOG_ERROR,"Unable to allocate memory for image components");
+                odcp_image_free(image);
+                return NULL;
+            }
+        }
+    }
+
+    /* set default image parameters 12-bit RGB */
+    image->bpp          = 12;
+    image->precision    = 12;
+    image->n_components = 3;
+    image->signed_bit   = 0;
+    image->dx           = 1;
+    image->dy           = 1;
+    image->w            = w;
+    image->h            = h;
+    image->x0           = 0;
+    image->y0           = 0;
+    image->x1 = !image->x0 ? (w - 1) * image->dx + 1 : image->x0 + (w - 1) * image->dx + 1;
+    image->y1 = !image->y0 ? (h - 1) * image->dy + 1 : image->y0 + (h - 1) * image->dy + 1;
+
+    return image;
+}
+
+/* free opendcp image structure */
+void odcp_image_free(odcp_image_t *odcp_image) {
+    int i;
+    if (odcp_image) {
+        if(odcp_image->component) {
+            for(i = 0; i < odcp_image->n_components; i++) {
+                odcp_image_component_t *component = &odcp_image->component[i];
+                if(component->data) {
+                    free(component->data);
+                }
+            }
+            free(odcp_image->component);
+        }
+        free(odcp_image);
+    }
+}
+
+int read_image(odcp_image_t **image, char *file) {
+    char *extension;
+    int  result;
+
+    extension = strrchr(file,'.');
+    extension++;
+
+    if (strncasecmp(extension,"tif",3) == 0) {
+        result = read_tif(image, file, 0);
+    } else if (strncasecmp(extension,"dpx",3) == 0) {
+        result = read_dpx(image, 0, file, 0);
+    } else if (strncasecmp(extension,"bmp",3) == 0) {
+        result = read_bmp(image, file, 0);
+    } else {
+        result = OPENDCP_ERROR;
+    }
+
+    if (result != OPENDCP_NO_ERROR) {
+        return OPENDCP_ERROR;
+    }
+
+    return OPENDCP_NO_ERROR;
+}
+
+int odcp_image_readline(odcp_image_t *image, int y, unsigned char *data) {
+    int x,i;
+    int d = 0;
+
+    for (x = 0; x<image->w; x+=2) {
+        i = (x+y+0) + ((image->w-1)*y);
+        data[d+0] = (image->component[0].data[i] >> 4);
+        data[d+1] = ((image->component[0].data[i] & 0x0f) << 4 )|((image->component[1].data[i] >> 8)& 0x0f);
+        data[d+2] = image->component[1].data[i];
+        data[d+3] = (image->component[2].data[i] >> 4);
+        data[d+4] = ((image->component[2].data[i] & 0x0f) << 4 )|((image->component[0].data[i+1] >> 8)& 0x0f);
+        data[d+5] = image->component[0].data[i+1];
+        data[d+6] = (image->component[1].data[i+1] >> 4);
+        data[d+7] = ((image->component[1].data[i+1] & 0x0f)<< 4 )|((image->component[2].data[i+1] >> 8)& 0x0f);
+        data[d+8] = image->component[2].data[i+1];
+        d+=9;
+    }
+
+    return OPENDCP_NO_ERROR;
+}
+
+int check_image_compliance(int profile, odcp_image_t *image, char *file) {
+    int          w,h;
+    odcp_image_t *odcp_image;
+
+    if (image == NULL) {
+        if (read_image(&odcp_image, file) == OPENDCP_NO_ERROR) {
+            h = odcp_image->h;
+            w = odcp_image->w;
+            odcp_image_free(odcp_image);
+        } else {
+            odcp_image_free(odcp_image);
+            return OPENDCP_ERROR;
+        }
+    } else {
+        h = image->h;
+        w = image->w;
+    }
+
+    switch (profile) {
+        case DCP_CINEMA2K:
+            if (!((w == 2048) | (h == 1080))) {
+                return OPENDCP_ERROR;
+            }
+        break;
+        case DCP_CINEMA4K:
+            if (!((w == 4096) | (h == 2160))) {
+                return OPENDCP_ERROR;
+            }
+            break;
+        default:
+            break;
+    }
+
+    return OPENDCP_NO_ERROR;
+}
+
+rgb_pixel_float_t yuv444toRGB888(int y, int cb, int cr) {
+    rgb_pixel_float_t p;
+
+    p.r = CLIP(y+1.402*(cr-128),255);
+    p.g = CLIP(y-0.344*(cb-128)-0.714*(cr-128),255);
+    p.b = CLIP(y+1.772*(cb-128),255);
+
+    return(p);
+}
+
+/* complex gamma function */
+float complex_gamma(float p, float gamma) {
+    float v;
+
+    if ( p > 0.04045) {
+        v = pow((p+0.055)/1.055,gamma);
+    } else {
+        v = p/12.92;
+    }
+
+    return v;
+}
+
+int rgb_to_xyz(odcp_image_t *image, int index, int method) {
+    int result;
+
+    if (method) {
+        dcp_log(LOG_DEBUG,"rgb_to_xyz_calculate");
+        result = rgb_to_xyz_calculate(image, index);
+    } else {
+        result = rgb_to_xyz_lut(image, index);
+    }
+
+    return result;
+}
+
+/* rgb to xyz color conversion 12-bit LUT */
+int rgb_to_xyz_lut(odcp_image_t *image, int index) {
+    int i;
+    int size;
+    rgb_pixel_float_t s;
+    xyz_pixel_float_t d;
+
+    size = image->w * image->h;
+
+    for (i=0;i<size;i++) {
+        /* in gamma lut */
+        s.r = lut_in[index][image->component[0].data[i]];
+        s.g = lut_in[index][image->component[1].data[i]];
+        s.b = lut_in[index][image->component[2].data[i]];
+
+        /* RGB to XYZ Matrix */
+        d.x = ((s.r * color_matrix[index][0][0]) + (s.g * color_matrix[index][0][1]) + (s.b * color_matrix[index][0][2]));
+        d.y = ((s.r * color_matrix[index][1][0]) + (s.g * color_matrix[index][1][1]) + (s.b * color_matrix[index][1][2]));
+        d.z = ((s.r * color_matrix[index][2][0]) + (s.g * color_matrix[index][2][1]) + (s.b * color_matrix[index][2][2]));
+
+        /* DCI Companding */
+        d.x = d.x * DCI_COEFFICENT * (DCI_LUT_SIZE-1);
+        d.y = d.y * DCI_COEFFICENT * (DCI_LUT_SIZE-1);
+        d.z = d.z * DCI_COEFFICENT * (DCI_LUT_SIZE-1);
+
+        /* out gamma lut */
+        image->component[0].data[i] = lut_out[LO_DCI][(int)d.x];
+        image->component[1].data[i] = lut_out[LO_DCI][(int)d.y];
+        image->component[2].data[i] = lut_out[LO_DCI][(int)d.z];
+    }
+
+    return OPENDCP_NO_ERROR;
+}
+
+/* rgb to xyz color conversion hard calculations */
+int rgb_to_xyz_calculate(odcp_image_t *image, int index) {
+    int i;
+    int size;
+    rgb_pixel_float_t s;
+    xyz_pixel_float_t d;
+
+    size = image->w * image->h;
+
+    for (i=0;i<size;i++) {
+        s.r = complex_gamma(image->component[0].data[i]/(float)COLOR_DEPTH, GAMMA[index]);
+        s.g = complex_gamma(image->component[1].data[i]/(float)COLOR_DEPTH, GAMMA[index]);
+        s.b = complex_gamma(image->component[2].data[i]/(float)COLOR_DEPTH, GAMMA[index]);
+
+        s.r = lut_in[index][image->component[0].data[i]];
+        s.g = lut_in[index][image->component[1].data[i]];
+        s.b = lut_in[index][image->component[2].data[i]];
+
+        d.x = ((s.r * color_matrix[index][0][0]) + (s.g * color_matrix[index][0][1]) + (s.b * color_matrix[index][0][2]));
+        d.y = ((s.r * color_matrix[index][1][0]) + (s.g * color_matrix[index][1][1]) + (s.b * color_matrix[index][1][2]));
+        d.z = ((s.r * color_matrix[index][2][0]) + (s.g * color_matrix[index][2][1]) + (s.b * color_matrix[index][2][2]));
+
+        image->component[0].data[i] = (pow((d.x*DCI_COEFFICENT),DCI_DEGAMMA) * COLOR_DEPTH);
+        image->component[1].data[i] = (pow((d.y*DCI_COEFFICENT),DCI_DEGAMMA) * COLOR_DEPTH);
+        image->component[2].data[i] = (pow((d.z*DCI_COEFFICENT),DCI_DEGAMMA) * COLOR_DEPTH);
+    }
+
+    return OPENDCP_NO_ERROR;
+}
+
+float b_spline(float x) {
+    float c = (float)1/(float)6;
+
+    if (x > 2.0 || x < -2.0) {
+        return 0.0f;
+    }
+
+    if (x < -1.0) {
+        return(((x+2.0f) * (x+2.0f) * (x+2.0f)) * c);
+    }
+
+    if (x < 0.0) {
+        return(((x+4.0f) * (x) * (-6.0f-3.0f*x)) * c);
+    }
+
+    if (x < 1.0) {
+        return(((x+4.0f) * (x) * (-6.0f+3.0f*x)) * c);
+    }
+
+    if (x < 2.0) {
+        return(((2.0f-x) * (2.0f-x) * (2.0f-x)) * c);
+    }
+
+    return 0;
+}
+
+/* get the pixel index based on x,y */
+static inline rgb_pixel_float_t get_pixel(odcp_image_t *image, int x, int y) {
+    rgb_pixel_float_t p;
+    int i;
+
+    i = x + (image->w*y);
+    //i = (x+y) + ((image->w-1)*y);
+
+    p.r = image->component[0].data[i];
+    p.g = image->component[1].data[i];
+    p.b = image->component[2].data[i];
+    
+    return p;
+} 
+
+int letterbox(odcp_image_t **image, int w, int h) {
+    int num_components = 3;
+    int x,y;
+    odcp_image_t *ptr = *image;
+    rgb_pixel_float_t p;
+
+    /* create the image */
+    odcp_image_t *d_image = odcp_image_create(num_components, w, h);
+
+    if (!d_image) {
+        return -1;
+    }
+
+    for(y=0; y<h; y++) {
+        for(x=0; x<w; x++) {
+            p = get_pixel(ptr, x, y);
+            d_image->component[0].data[0] = (int)p.r;
+            d_image->component[1].data[0] = (int)p.g;
+            d_image->component[2].data[0] = (int)p.b;
+         }
+    }
+
+
+    odcp_image_free(*image);
+    *image = d_image;
+
+    return OPENDCP_NO_ERROR;
+} 
+
+/* resize image */
+int resize(odcp_image_t **image, int profile, int method) {
+    int num_components = 3;
+    odcp_image_t *ptr = *image;
+    rgb_pixel_float_t p;
+    int w,h;
+    float aspect;
+  
+    /* aspect ratio */
+    aspect = (float)ptr->w/(float)ptr->h;
+
+    /* depending on the aspect ratio, set height or weight priority */
+    if (aspect <= 2.10) {
+        w = (ptr->w*MAX_HEIGHT_2K/ptr->h);
+        h = MAX_HEIGHT_2K;
+    } else {
+        w = MAX_WIDTH_2K;
+        h = (ptr->h*MAX_WIDTH_2K/ptr->w);
+    }
+
+    /* if we overshot width, scale back */
+    if (w > MAX_WIDTH_2K) {
+        w = MAX_WIDTH_2K;
+        h = w / aspect;
+    }
+
+    /* if we overshot height, scale back */
+    if (h > MAX_HEIGHT_2K) {
+        h = MAX_HEIGHT_2K;
+        w = h * aspect;
+    }
+
+    /* adjust for 4K */
+    if (profile == DCP_CINEMA4K) {
+        w *= 2;
+        h *= 2;
+    }
+
+    dcp_log(LOG_INFO,"Resizing from %dx%d to %dx%d (%f)",ptr->w, ptr->h,w,h,aspect);
+
+    /* create the image */
+    odcp_image_t *d_image = odcp_image_create(num_components, w, h);
+
+    if (!d_image) {
+        return -1;
+    }
+
+    /* simple resize - pixel double */
+    if (method == NEAREST_PIXEL) {
+        int x,y,i,dx,dy;
+        float tx, ty;
+
+        tx = (float)ptr->w / w;
+        ty = (float)ptr->h / h;
+
+        for(y=0; y<h; y++) {
+            dy = y * ty; 
+            for(x=0; x<w; x++) {
+                dx = x * tx;
+                p = get_pixel(ptr, dx, dy);
+                i = x + (w*y);
+                d_image->component[0].data[i] = (int)p.r;
+                d_image->component[1].data[i] = (int)p.g;
+                d_image->component[2].data[i] = (int)p.b;
+             }
+         }
+    }
+
+    odcp_image_free(*image);
+    *image = d_image; 
+
+    return OPENDCP_NO_ERROR;
+}
